eec60fd8f42cd1a39fae02eb40994df80423fe1a
Ludde86 <bslbjorn@gmail.com>
2020-10-13 09:10:16 +0200

    add stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { HomeScreen, LoginScreen, RegistrationScreen } from './src/screens';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const [loading, setLoading] = useState(true)
+const [user, setUser] = useState(null)
+
+return(
+  <NavigationContainer>
+    <Stack.Navigator>
+      {user ? (
+        <Stack.Screen name="Home">
+          {props => <HomeScreen {...props} extraData={user} />}
+        </Stack.Screen>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Registration" component={RegistrationScreen} />
+        </>
+      )}
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+}
